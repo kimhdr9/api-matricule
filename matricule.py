844922086feaa32874562de27f5613aa9e0e7a69
kim do <kimhdr9@gmail.com>
2024-02-18 17:39:23 +0100

    invite pour la plaque

--- matricule.py
+++ matricule.py
@@ -3,6 +3,10 @@ import json
 hostname="https://apiplaqueimmatriculation.com"
 immatriculation="EJ-941-ZH"
 immatriculation="CY-648-SH"
+immatriculation="AC-471-VF"
+
+print('Entrer le matricule avec le format AB-123-CD')
+immatriculation= input()
 
 token='TokenDemo'
 format='json'
